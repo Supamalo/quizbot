7bd54b9082a90457c1bcecbd1378df8db17e228d
Vasiliy <57080854+Supamalo@users.noreply.github.com>
2025-08-11 10:55:09 +0200

    Update quizLogic.js
    change double string

--- quizLogic.js
+++ quizLogic.js
@@ -152,7 +152,7 @@ ${durationStr}`;
         const durationStr = durationMs !== null
           ? `Длительность: ${Math.floor(durationMs / 1000)} сек.`
           : '';
-        const messageText = `Квиз пройден: ${user.firstName} ${user.lastName} (@${user.username || 'Unknown'})
+        const messageText = `Квиз пройден: ${user.firstName} ${user.lastName}${user.username ? ` (@${user.username})` : ''}
 Тема: ${quizName}
 Результат: ${score} из ${total}
 Время начала: ${startedAt}
